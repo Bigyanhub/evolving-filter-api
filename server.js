b0457a97d33e61e1eae4cd491745b6c92237d2b2
Bigyan Basnet <bigyanbasnet849@gmail.com>
2025-07-16 17:12:49 +0545

    Basic data filtering

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { startups } from './data/data.js'
+
+const PORT = 8000
+
+const app = express()
+
+app.get('/api', (req, res) => {
+
+  let filteredData = startups
+
+ const{ industry, country, continent, is_seeking_funding, has_mvp }= req.query
+ 
+if(industry) {
+  filteredData = filteredData.filter(obj => obj.industry.toLowerCase() === industry.toLowerCase())
+}
+
+if(country){
+  filteredData = filteredData.filter(obj => obj.country.toLowerCase() === country.toLowerCase())
+}
+
+if(continent){
+  filteredData = filteredData.filter(obj=> obj.continent.toLowerCase() === continent.toLowerCase())
+}
+
+if(is_seeking_funding){
+  filteredData = filteredData.filter(obj=> obj.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase()))
+}
+
+if(has_mvp){
+  filteredData = filteredData.filter(obj => obj.has_mvp === JSON.parse(has_mvp.toLowerCase()))
+}
+
+if(filteredData.length === 0) {
+  return res.status(404).json({ message: 'No startups found with the given criteria.' })
+}
+res.json(filteredData)
+
+})
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
+
+
